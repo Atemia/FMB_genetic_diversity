be3786222ef53500161316a56590f031c71a9609
Atemia <46397693+Atemia@users.noreply.github.com>
2020-08-26 18:04:44 +0300

    Update tree.sh

---
+++ phylogenetics/tree.sh
@@ -0,0 +1,4 @@
+# Host:    pubrec-cls-01 (AVX512, FMA3, 30 GB RAM)
+# Command: 
+iqtree -s phlip_new_fasta.phy -nt AUTO -m MFP -bb 1000
+# Seed:    751228 (Using SPRNG - Scalable Parallel Random Number Generator)
